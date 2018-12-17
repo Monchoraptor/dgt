83242ea25b0fc3bd38e4d9b80ec21b8735352c1c
Adri <amora@DESKTOP-AUH0J3M>
2018-12-17 16:10:18 +0100

    Inicio diseño inicial hecho

--- src/dgt/Dgt.java
+++ src/dgt/Dgt.java
@@ -15,7 +15,8 @@ public class Dgt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pantalla_Inicio pa=new Pantalla_Inicio();
+        pa.setVisible(true);
     }
     
 }
